4e1b7e3415503a54c1ef28f5afeb8e1a746b79ff
Steven Tseng <booda406@gmail.com>
2014-06-02 15:44:31 +0800

    fix photo

--- js/script.js
+++ js/script.js
@@ -15,13 +15,10 @@ FB.getLoginStatus(function(response) {
     var uid = response.authResponse.userID;
     var accessToken = response.authResponse.accessToken;
     FB.api('/me/picture?type=normal', function(response) { // normal/large/squere 
-		var str="<img src="+ response.data.url +">";
-		$('#preview1').html(str);
-		$('#name').append(str);
+		$('#photo').attr("src") = response.data.url;
 	});
     FB.api('/me', function (response) {
-		var name = "<h1>" + response.name + "</h1>";
-		$('#name').append(name);
+		$('#name').innerHTML = response.name;
     });
     // console.log(response);
     
@@ -103,7 +100,7 @@ FB.getLoginStatus(function(response) {
       // if the drag flag is set, clear the canvas and draw the image
       if(isDragging){ //當拖拉為True時
           	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-			var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
+			var profileIMG = document.getElementById("photo");//抓html裡預載入的照片
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			canvas.height = profileIMG.height;
