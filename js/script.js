a615f1b784d7cf98b32ad95261d2935a98a826c1
Steven Tseng <booda406@gmail.com>
2014-06-02 17:18:13 +0800

    check permission

--- js/script.js
+++ js/script.js
@@ -10,8 +10,16 @@ window.fbAsyncInit = function () {//facebook init
         });
 
 FB.getLoginStatus(function(response) {
-	console.log(response);
   if (response.status === 'connected') {
+  	FB.api(
+    "/me/permissions",
+    function (response) {
+      if (response && !response.error) {
+        /* handle the result */
+        console.log(response);
+      }
+    }
+);
     //呼叫api把圖片放到#preview IMG tag 內
     var uid = response.authResponse.userID;
     var accessToken = response.authResponse.accessToken;
@@ -33,7 +41,7 @@ FB.getLoginStatus(function(response) {
         	window.location.reload();
         };
 	 }, {
-	   scope: 'publish_actions', 
+	   scope: 'publish_actions,user_photos', 
 	   return_scopes: true
 	 });	
   } else {
@@ -44,7 +52,7 @@ FB.getLoginStatus(function(response) {
         	window.location.reload();
         };
 	 }, {
-	   scope: 'publish_actions', 
+	   scope: 'publish_actions,user_photos', 
 	   return_scopes: true
 	 });
    }
@@ -108,7 +116,7 @@ FB.getLoginStatus(function(response) {
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			// canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			// canvas.height = profileIMG.height;
-			ctx.drawImage(profileIMG,240,240);//從XY軸0，0值開始畫如profileimg
+			ctx.drawImage(profileIMG,200,200);//從XY軸0，0值開始畫如profileimg
 			ctx.drawImage(img3,canMouseX-128/2,canMouseY-120/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			ctx.drawImage(img2,0,0); //劃入img2
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
@@ -130,15 +138,12 @@ FB.getLoginStatus(function(response) {
 
 
 //LOAD FACEBOOK SDK ASYNC，這是基本的東西，應該不用多說了吧
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js"; 
-    fjs.parentNode.insertBefore(js, fjs);
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=581530521961724&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 
