7e2b958e50d6ed6254075b73b1ad7065458184fd
Steven Tseng <booda406@gmail.com>
2014-06-02 16:50:30 +0800

    check permission

--- js/script.js
+++ js/script.js
@@ -10,11 +10,12 @@ window.fbAsyncInit = function () {//facebook init
         });
 
 FB.getLoginStatus(function(response) {
+	console.log(response);
   if (response.status === 'connected') {
     //呼叫api把圖片放到#preview IMG tag 內
     var uid = response.authResponse.userID;
     var accessToken = response.authResponse.accessToken;
-    FB.api('/me/picture?type=normal', function(response) { // normal/large/squere 
+    FB.api('/me/picture?type=large', function(response) { // normal/large/squere 
 		$('#photo').attr("src", response.data.url);
 	});
     FB.api('/me', function (response) {
@@ -107,7 +108,7 @@ FB.getLoginStatus(function(response) {
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			// canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			// canvas.height = profileIMG.height;
-			ctx.drawImage(profileIMG,256,256);//從XY軸0，0值開始畫如profileimg
+			ctx.drawImage(profileIMG,240,240);//從XY軸0，0值開始畫如profileimg
 			ctx.drawImage(img3,canMouseX-128/2,canMouseY-120/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			ctx.drawImage(img2,0,0); //劃入img2
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
@@ -147,7 +148,7 @@ FB.getLoginStatus(function(response) {
 // Post a BASE64 Encoded PNG Image to facebook，以下程式為把照片po到facebook的方法，基本上這樣就可以不用動了，但思考authToken該怎麼拿到，因為這裡我並沒有把使用者登入的token載入到這函數內，所以它是不會得到token的
 function PostImageToFacebook(authToken) {
 	authToken = $('#accesstoken').html();
-	console.log(authToken);
+	// console.log(authToken);
 	$('.info').append('<img src="img/loading.gif"/>')//載入loading的img
     var canvas = document.getElementById("canvas");//找canvas
     var imageData = canvas.toDataURL("image/png");//把canvas轉換PNG
