33ebd072957f127b8444fbcd8edb7b873df3eb0c
Steven Tseng <booda406@gmail.com>
2014-06-04 02:41:28 +0800

    fix word frame

--- js/script.js
+++ js/script.js
@@ -219,7 +219,7 @@ FB.getLoginStatus(function(response) {
 			ctx.drawImage(img3,canMouseX,canMouseY+50); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			ctx.drawImage(img2,0,0); //劃入img2
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
-			ctx.fillStyle = "white"; //字體顏色
+			ctx.fillStyle = "black"; //字體顏色
 			ctx.font='20px "微軟正黑體"'; //字體大小和字形
 			ctx.fillText(inputedText, canMouseX+60, canMouseY+100); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
       }
