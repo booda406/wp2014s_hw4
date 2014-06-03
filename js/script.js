61c8d9b92685f18fff27df3aa5b81d307aeb884a
Steven Tseng <booda406@gmail.com>
2014-06-04 02:20:30 +0800

    auto change img2 width height

--- js/script.js
+++ js/script.js
@@ -213,6 +213,8 @@ FB.getLoginStatus(function(response) {
           	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 			var profileIMG = document.getElementById("photo");//抓html裡預載入的照片
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
+			img2.style.width = profileIMG.width;
+			img2.style.height = profileIMG.height;
 			canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			canvas.height = profileIMG.height;
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
