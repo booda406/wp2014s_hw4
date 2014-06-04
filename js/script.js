4450cffd3f21c18b9d7bd2e77be7d7ecba107ad2
Steven Tseng <booda406@gmail.com>
2014-06-04 16:37:46 +0800

    add select frame word

--- js/script.js
+++ js/script.js
@@ -192,6 +192,8 @@ FB.getLoginStatus(function(response) {
     var canvasWidth=canvas.width;//大小
     var canvasHeight=canvas.height;//高度
     var isDragging=false;//拖拉
+    var frame=false;
+    var word=true;
 
     function handleMouseDown(e){//滑鼠按下的函數
       canMouseX=parseInt(e.clientX-offsetX);//抓滑鼠游標X
@@ -226,18 +228,28 @@ FB.getLoginStatus(function(response) {
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			canvas.height = profileIMG.height;
-			img2.width = canvas.width;
-			img2.height = canvas.height;
-			console.log(img2.width);
-			console.log(img2.height);
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
-			ctx.drawImage(img3,canMouseX-30,canMouseY+80); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
-			ctx.drawImage(img2,0,0); //劃入img2
-			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
-			ctx.fillStyle = "black"; //字體顏色
-			ctx.font='20px "微軟正黑體"'; //字體大小和字形
-			ctx.fillText(inputedText, canMouseX+30, canMouseY+130); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
+			if(word){
+				ctx.drawImage(img3,canMouseX-30,canMouseY+80); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
+				var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
+				ctx.fillStyle = "black"; //字體顏色
+				ctx.font='20px "微軟正黑體"'; //字體大小和字形
+				ctx.fillText(inputedText, canMouseX+30, canMouseY+130); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
+			}
+			if(frame){
+				ctx.drawImage(img2,canMouseX-30,canMouseY+80); //劃入img2
+			}
       }
+    }
+
+    function move_frame(){
+    	frame=true;
+    	word=false;
+    }
+
+    function move_word(){
+    	frame=false;
+    	word=true;
     }
 
 	//抓取滑鼠移動的event
