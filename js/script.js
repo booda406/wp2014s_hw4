0e93f687ce7a275e393cea2bd04da967bf8237c7
Steven Tseng <booda406@gmail.com>
2014-06-04 16:21:50 +0800

    add preload and img2.size

--- js/script.js
+++ js/script.js
@@ -226,6 +226,10 @@ FB.getLoginStatus(function(response) {
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			canvas.height = profileIMG.height;
+			img2.width = canvas.width;
+			img2.height = canvas.height;
+			console.log(img2.width);
+			console.log(img2.height);
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
 			ctx.drawImage(img3,canMouseX-30,canMouseY+80); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 			ctx.drawImage(img2,0,0); //劃入img2
