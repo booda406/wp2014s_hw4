a28e67cdc57245b7b9586aad4d4fdc2c666da13e
Steven Tseng <booda406@gmail.com>
2014-06-06 00:54:11 +0800

    change anonymous

--- js/script.js
+++ js/script.js
@@ -234,7 +234,7 @@ FB.getLoginStatus(function(response) {
       		img3.src = $('#word').val();
           	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 			var profileIMG = document.getElementById("photo");//抓html裡預載入的照片
-			profileIMG.crossOrigin = "*"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
+			profileIMG.crossOrigin = "anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			canvas.height = profileIMG.height;
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
