89d1c7e2f7b3a1515b838a03cd2d358821883664
Steven Tseng <booda406@gmail.com>
2014-06-07 21:57:06 +0800

    add move pic

--- js/script.js
+++ js/script.js
@@ -57,10 +57,7 @@ FB.getLoginStatus(function(response) {
      });
    }
  });
-function hi(){
-  alert("hi");
-  console.log("hi");
-}
+
 }; //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<init end
  
 
@@ -202,7 +199,10 @@ function hi(){
     var canvasHeight=canvas.height;//高度
     var isDragging=false;//拖拉
     var frame=false;
-    var word=true;
+    var word=false;
+    var pic=true;
+    var a = 0;
+    var b = 0;
 	  var x = 0;
 	  var y = 0;
 
@@ -239,8 +239,13 @@ function hi(){
 			//profileIMG.crossOrigin = "anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			// canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			// canvas.height = profileIMG.height;
-			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
+			if(pic){
+        ctx.drawImage(profileIMG,canMouseX,canMouseY);//從XY軸0，0值開始畫如profileimg
+        a = canMouseX;
+        b = canMouseY;
+      }
 			if(word){
+        ctx.drawImage(profileIMG,a,b);//從XY軸0，0值開始畫如profileimg
 				ctx.drawImage(img3,canMouseX-130,canMouseY+20); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 				var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 				ctx.fillStyle = "black"; //字體顏色
@@ -250,6 +255,7 @@ function hi(){
 				y = canMouseY;
 			}
 			if(frame){
+        ctx.drawImage(profileIMG,a,b);//從XY軸0，0值開始畫如profileimg
 				ctx.drawImage(img2,canMouseX-170,canMouseY-20); //劃入img2
 				ctx.drawImage(img3,x-130,y+20); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 				var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
@@ -259,15 +265,21 @@ function hi(){
 			}
       }
     }
-
+    function move_pic(){
+      frame=false;
+      word=false;
+      pic=true;
+    }
     function move_frame(){
     	frame=true;
     	word=false;
+      pic=false;
     }
 
     function move_word(){
     	frame=false;
     	word=true;
+      pic=false;
     }
 
 	//抓取滑鼠移動的event
