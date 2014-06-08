6f955eebfc5a6e8fdcb17bbc6412d11a0459d4c0
Steven Tseng <booda406@gmail.com>
2014-06-08 22:46:51 +0800

    fix cros

--- js/script.js
+++ js/script.js
@@ -242,7 +242,7 @@ function canvas(){
       		img3.src = $('#word').val();
           	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 			var profileIMG = document.getElementById("photo");//抓html裡預載入的照片
-			profileIMG.crossOrigin = "anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
+			// profileIMG.crossOrigin = "anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			// canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			// canvas.height = profileIMG.height;
 			if(window.pic){
